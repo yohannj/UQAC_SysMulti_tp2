608178c92e8ccc5d6c897f641b357ac0c0ee26e0
Yohann Jardin <yohann.jardin@telecomnancy.net>
2015-02-25 23:48:40 -0500

    Début de substitution de x et y, non satisfait par cette méthode, il faut mieux gérer ça dans le parser.

--- src/MoteurInference.java
+++ src/MoteurInference.java
@@ -46,7 +46,10 @@ public class MoteurInference {
         //1. Obtenir les faits initiaux
         initFaits();
 
-        List<Regle> regles_non_marquees = new ArrayList<Regle>(regles);
+        List<Regle> regles_non_marquees = new ArrayList<Regle>();
+        for(int i = 0; i < regles.size(); ++i) {
+            regles_non_marquees.add(new Regle(regles.get(i)));
+        }
         regles_non_marquees.parallelStream()
                 .forEach(r -> r.adapteAuDernierPoint(dernier_coup.x, dernier_coup.y));
         termine = false;
@@ -54,6 +57,7 @@ public class MoteurInference {
         //2. Tant que (Pas terminé et il reste au moins une règle non marquée) faire
         while (!termine && !regles_non_marquees.isEmpty()) {
             List<Regle> regle_applicable = new ArrayList<Regle>();
+            Map<Regle, Point> caseQuiValideLaRegle = new HashMap<Regle, Point>();
 
             //2.1 Sélectionner les règles applicables : celles non marquées
             for (Iterator<Regle> i = regles_non_marquees.iterator(); i.hasNext();) {
@@ -65,17 +69,20 @@ public class MoteurInference {
                 } else {
 
                     //2.1 Sélectionner les règles applicables : celles dont les conditions existent dans la base de faits
-                    if (r.satisfaitConditions(faits)) {
+                    Point p = r.satisfaitConditions(faits);
+                    if (p != null) {
                         regle_applicable.add(r);
+                        caseQuiValideLaRegle.put(r, p);
                     }
                 }
             }
 
             //2.2 Choisir la règle à appliquer (arbitrairement, ou autre)
             Regle regle_appliquee = regle_applicable.get(0);
+            Point caseValidantLaRegle = caseQuiValideLaRegle.get(regle_appliquee);
 
             //2.3 Appliquer la règle: ajouter les conclusions à la base de faits
-            appliquerRegle(regle_appliquee);
+            appliquerRegle(regle_appliquee, caseValidantLaRegle.x, caseValidantLaRegle.y, null); //TODO
 
             //2.4 Marquer la règle
             regles_non_marquees.remove(regle_appliquee);
@@ -109,86 +116,82 @@ public class MoteurInference {
      */
     public void majCarteEtDernierPoint(char[][] carte) {
         this.carte = carte;
-        if(carte[dernier_coup.x][dernier_coup.y] == 'o' && carte[avant_dernier_coup.x][avant_dernier_coup.y] != 'o' && carte[avant_dernier_coup.x][avant_dernier_coup.y] != 'v') {
+        if (carte[dernier_coup.x][dernier_coup.y] == 'o'
+                && carte[avant_dernier_coup.x][avant_dernier_coup.y] != 'o'
+                && carte[avant_dernier_coup.x][avant_dernier_coup.y] != 'v') {
             dernier_coup = avant_dernier_coup;
         }
     }
 
-<<<<<<< HEAD
-    private void appliquerRegle(Regle r,int x, int y, String t) {
-    	int x_cible,y_cible;
-    	int i = 1;
-		boolean trouv = false;
-=======
-    private void appliquerRegle(Regle r) { //Conseil : Utiliser javax.script.ScriptEngineManager et javax.script.ScriptEngine. (Attention ? ils renvoient un double, peut-être faire le passage en int) -> http://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form
->>>>>>> f19c7ede564baee8a70d0318fdff3a908f5d64b4
+    private void appliquerRegle(Regle r, int x, int y, String t) { //Conseil : Utiliser javax.script.ScriptEngineManager et javax.script.ScriptEngine. (Attention ? ils renvoient un double, peut-être faire le passage en int) -> http://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form
+        int x_cible, y_cible;
+        int i = 1;
+        boolean trouv = false;
         for (String s : r.getConsequence()) {
             if (s.contains("jouer")) {
                 termine = true;
                 if (s.matches("jouer(.*;.*)")) {
-<<<<<<< HEAD
-                    //TODO Ajouter le fait jouer(x,y) dans les faits.
-                	String chaine1 = s.substring(7, s.length()-1).split(";")[0];
-                	String chaine2 = s.substring(7, s.length()-1).split(";")[1].replace(")", "");
-                	x_cible =valeur_dep(chaine1);
-                	y_cible =valeur_dep(chaine2);
-                	faits.add("jouer("+x_cible+";"+y_cible+")");
-                } else if (s.contains("last")){
-=======
                     //TODO Ajouter le fait jouer(x,y) dans les faits. (Il ne faut plus de calcul dedans)
+                    String chaine1 = s.substring(7, s.length() - 1).split(";")[0];//XXX Revoir le 7, ce serait plutôt 6 non ?
+                    String chaine2 = s.substring(7, s.length() - 1).split(";")[1].replace(")", "");//XXX Revoir le 7, ce serait plutôt 6 non ?
+                    x_cible = valeur_dep(chaine1);
+                    y_cible = valeur_dep(chaine2);
+                    faits.add("jouer(" + x_cible + ";" + y_cible + ")");
                 } else if (s.contains("last")) {
->>>>>>> f19c7ede564baee8a70d0318fdff3a908f5d64b4
                     //TODO On est dans le cas jouer(last_gauche) || jouer(last_haut) || jouer(last_droite). Déterminer (à l'aide d'une nouvelle fonction ?) x,y pour pouvoir ajouter jouer(x,y) dans les faits.
-                	if(s.contains("gauche")){
-                		while(!trouv){
-                			if(carte[x][y-i]=='v'){
-                				y_cible = y-i;
-                				trouv = true;
-                            	faits.add("jouer("+x+";"+y_cible+")");
-                			}else{
-                				i++;
-                			}
-                		}
-                	}else if(s.contains("haut")){
-                		while(!trouv){
-                			if(carte[x+i][y]=='v'){
-                				x_cible = x+i;
-                				trouv = true;
-                            	faits.add("jouer("+x_cible+";"+y+")");
-                			}else{
-                				i++;
-                			}
-                		}
-                	}else if(s.contains("bas")){
-                		while(!trouv){
-                			if(carte[x-i][y]=='v'){
-                				x_cible = x-i;
-                				trouv = true;
-                            	faits.add("jouer("+x_cible+";"+y+")");
-                			}else{
-                				i++;
-                			}
-                		}
-                	}
+                    if (s.contains("gauche")) {
+                        while (!trouv) {
+                            if (carte[x][y - i] == 'v') {
+                                y_cible = y - i;
+                                trouv = true;
+                                faits.add("jouer(" + x + ";" + y_cible + ")");
+                            } else {
+                                i++;
+                            }
+                        }
+                    } else if (s.contains("haut")) {
+                        while (!trouv) {
+                            if (carte[x + i][y] == 'v') {
+                                x_cible = x + i;
+                                trouv = true;
+                                faits.add("jouer(" + x_cible + ";" + y + ")");
+                            } else {
+                                i++;
+                            }
+                        }
+                    } else if (s.contains("bas")) {
+                        while (!trouv) {
+                            if (carte[x - i][y] == 'v') {
+                                x_cible = x - i;
+                                trouv = true;
+                                faits.add("jouer(" + x_cible + ";" + y + ")");
+                            } else {
+                                i++;
+                            }
+                        }
+                    }
                 } else {
                     //TODO On est dans le cas jouer(aleatoire). Déterminer x,y depuis un fait "inconnu(x,y)" pour pouvoir ajouter jouer(x,y) dans les faits.
-                	Iterator iterator = faits.iterator();
-                	while(iterator.hasNext()&&!trouv){
-                		String element = (String) iterator.next();
-                		if(element.matches("jouer(.*;.*)")){
-                			trouv = true;
-                			x_cible = Integer.parseInt(element.substring(8, element.length()-1).split(";")[0]);
-                			y_cible = Integer.parseInt(element.substring(8, element.length()-1).split(";")[1].replace(")", ""));
-                        	faits.add("jouer("+x_cible+";"+y_cible+")");
-                		}
-                		
-                	}
+                    Iterator<String> iterator = faits.iterator();
+                    while (iterator.hasNext() && !trouv) {
+                        String element = (String) iterator.next();
+                        if (element.matches("jouer(.*;.*)")) {
+                            trouv = true;
+                            x_cible = Integer.parseInt(element.substring(8, element.length() - 1)
+                                    .split(";")[0]);
+                            y_cible = Integer.parseInt(element.substring(8, element.length() - 1)
+                                    .split(";")[1].replace(")", ""));
+                            faits.add("jouer(" + x_cible + ";" + y_cible + ")");
+                        }
+
+                    }
                 }
             } else {
                 //TODO Rajout des conséquences dans les faits.
-            	for(String ch : s.split("=>")[1].replace(" ", "").split(",")){
-            		faits.add(ch.replace("x", Integer.toString(x)).replace("y", Integer.toString(y)));
-            	}
+                for (String ch : s.split("=>")[1].replace(" ", "").split(",")) { //XXX s ne peut pas contenir "=>" ni d'espace, ni de virgule. Il faut qu'on en reparle.
+                    faits.add(ch.replace("x", Integer.toString(x))
+                            .replace("y", Integer.toString(y)));
+                }
             }
         }
     }
@@ -212,7 +215,7 @@ public class MoteurInference {
      * @return Si la règle est en contradiction avec les faits, true
      */
     private boolean enContradiction(Regle r) { //Conseil : Utiliser javax.script.ScriptEngineManager et javax.script.ScriptEngine. (Attention ? ils renvoient un double, peut-être faire le passage en int) -> http://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form
-        //TODO Compléter en vérifiant les non_
+        //Devrais fonctionner en utilisant uniquement false
         return false;
     }
 
@@ -250,14 +253,17 @@ public class MoteurInference {
 
         return liste;
     }
-    private int valeur_dep(String chaine){
-    	if(chaine.contains("+")){
-    		int dep = Integer.parseInt(chaine.split("+")[1]);
-    		return dernier_coup.x+dep;
-    	}else if(chaine.contains("-")){
-    		int dep = Integer.parseInt(chaine.split("-")[1]);
-    		return dernier_coup.x-dep;
-    	}
-    	return 0;
+
+    private int valeur_dep(String chaine) {
+        //TODO le calcul peut ne pas être avec dernier_coup.x mais aussi avec y. Utiliser dernier_coup.x (ou .y) n'est pas trop scalable non plus :/
+        //Rappel : "Conseil : Utiliser javax.script.ScriptEngineManager et javax.script.ScriptEngine. (Attention ? ils renvoient un double, peut-être faire le passage en int) -> http://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form"
+        if (chaine.contains("+")) {
+            int dep = Integer.parseInt(chaine.split("+")[1]);
+            return dernier_coup.x + dep;
+        } else if (chaine.contains("-")) {
+            int dep = Integer.parseInt(chaine.split("-")[1]);
+            return dernier_coup.x - dep;
+        }
+        return 0;
     }
 }
--- src/Regle.java
+++ src/Regle.java
@@ -1,7 +1,13 @@
+import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 
 public class Regle {
 
@@ -11,6 +17,11 @@ public class Regle {
     public Regle() {
         premisses = new ArrayList<String>();
         consequences = new ArrayList<String>();
+    }
+
+    public Regle(Regle r) {
+        premisses = new ArrayList<String>(r.premisses);
+        consequences = new ArrayList<String>(r.consequences);
     }
 
     public void addPremisse(String fait) {
@@ -40,36 +51,70 @@ public class Regle {
 
     /**
      * @param faits
-     * @return
-     * Permet de savoir si un ensemble de faits satisfait tous les prémisses d’une règle. 
+     * @return null si non satisfait. Sinon le Point(x;y) validant la règle, le Point n'est pas instancié s'il n'est pas nécessaire pour la lecture de la règle
      */
-    public boolean satisfaitConditions(Set<String> faits) {
+    public Point satisfaitConditions(Set<String> faits) {
+        Map<Point, Integer> nbPremissesValideesPar = new HashMap<Point, Integer>();
+
         for (String s : premisses) {
-            boolean satisfied = false;
-            for (Iterator<String> i = faits.iterator(); !satisfied
-                    && i.hasNext();) {
-                if (s.equals(i.next()))
-                    satisfied = true;
+            for (Iterator<String> i = faits.iterator(); i.hasNext();) {
+                String fait = i.next();
+                List<Object> param = récupérerParamètreDuFait(fait);
+                if (s.equals(i.next())) {
+                    
+                }
             }
-            if (!satisfied)
-                return false;
+            //if (!satisfied)
+              //  return null;
         }
-        return true;
+        return null;
     }
-    
+
     /**
      * @param dernierX
      * @param dernierY
      * Remplace toutes les occurences de "dernierX" et "dernierY" par leurs valeurs 
      */
     public void adapteAuDernierPoint(int dernierX, int dernierY) {
-        for(int i = 0; i < premisses.size(); ++i) {
-            premisses.set(i, premisses.get(i).replaceAll("dernierX", ""+dernierX));
-            premisses.set(i, premisses.get(i).replaceAll("dernierY", ""+dernierY));
+        for (int i = 0; i < premisses.size(); ++i) {
+            premisses.set(i, premisses.get(i).replaceAll("dernierX", ""
+                    + dernierX));
+            premisses.set(i, premisses.get(i).replaceAll("dernierY", ""
+                    + dernierY));
         }
-        for(int i = 0; i < premisses.size(); ++i) {
-            consequences.set(i, consequences.get(i).replaceAll("dernierX", ""+dernierX));
-            consequences.set(i, consequences.get(i).replaceAll("dernierY", ""+dernierY));
+        for (int i = 0; i < premisses.size(); ++i) {
+            consequences.set(i, consequences.get(i).replaceAll("dernierX", ""
+                    + dernierX));
+            consequences.set(i, consequences.get(i).replaceAll("dernierY", ""
+                    + dernierY));
+        }
+    }
+
+    private List<Object> récupérerParamètreDuFait(String fait) {
+        List<Object> res = new ArrayList<Object>();
+        String[] faitSplit = fait.split(";");
+        int x = calcString(faitSplit[0].split("(")[1]);
+        int y;
+        String t = null;
+        if (faitSplit.length == 2) {
+            y = calcString(faitSplit[1].split(")")[0]);
+        } else {
+            y = calcString(faitSplit[1]);
+            t = faitSplit[2].split(")")[0];
+        }
+        res.add(x);
+        res.add(y);
+        res.add(t);
+        return res;
+    }
+
+    private int calcString(String s) {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
+        try {
+            return (int) engine.eval(s);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+            return -1;
         }
     }
 }
